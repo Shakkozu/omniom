463b932acc41425d0a311cc44d6640cb7748f716
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-03-09 10:01:43 +0100

    Refactor process which verifies product quantity during products catalogue seed

---
+++ src/Omniom/Omniom.Domain/ProductsCatalogue/SeedDatabase/QuantityCorrector.cs
@@ -0,0 +1,67 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Omniom.Domain.ProductsCatalogue.SeedDatabase;
+public static class QuantityCorrector
+{
+    private sealed record UnitTranslation(string Unit, decimal ConversionRatio);
+
+    private static Regex _regex = new Regex("([\\d,\\.]*)\\s?(g|ml|l|kg)?", RegexOptions.Compiled);
+
+    private static List<UnitTranslation> _unitTranslations = [
+        new ("ml", 1),
+        new ("l", 1000),
+    ];
+
+    public record QuantityCorrectionResults 
+    {
+        public static QuantityCorrectionResults CorrectResultFrom(decimal value) => new QuantityCorrectionResults((int)value, string.Empty);
+        public static QuantityCorrectionResults IncorrectResult(string error) => new QuantityCorrectionResults(default, error);
+        public QuantityCorrectionResults(int result, string error)
+        {
+            Result = result;
+            Error = error;
+        }
+        public int Result { get; init; }
+        public string Error { get; init; }
+
+        public bool HasError => !string.IsNullOrEmpty(Error);
+    }
+
+    public static QuantityCorrectionResults ConvertQuantitySizeTextToNumericValueSpecifiedInGrams(string entry)
+    {
+        var corrected = entry.Trim().ToLower().Replace(".", ",");
+        decimal numericValue;
+        if (decimal.TryParse(corrected, out numericValue))
+        {
+            return QuantityCorrectionResults.CorrectResultFrom(numericValue);
+        }
+        string valueGroup;
+
+        var match = _regex.Match(corrected);
+        if (!match.Success || match.Groups.Count < 2)
+            return QuantityCorrectionResults.IncorrectResult($"Provided quantity entry is not correct. Entry: {entry}");
+
+        if (match.Groups.Count < 3 && corrected.All(ch => char.IsDigit(ch)))
+        {
+            valueGroup = match.Groups[1].Value;
+            decimal.TryParse(valueGroup.Replace(".", ","), out numericValue);
+            return QuantityCorrectionResults.CorrectResultFrom(numericValue);
+        }
+
+        valueGroup = match.Groups[1].Value;
+        var unit = match.Groups[2].Value;
+        decimal.TryParse(valueGroup.Replace(".", ","), out numericValue);
+        if (unit.Trim() == "g")
+        {
+            return QuantityCorrectionResults.CorrectResultFrom(numericValue);
+        }
+
+        var translationUnit = _unitTranslations.FirstOrDefault(unit => corrected.Contains(unit.Unit));
+        if (translationUnit != null)
+        {
+            return QuantityCorrectionResults.CorrectResultFrom(numericValue * translationUnit.ConversionRatio);
+        }
+
+        return QuantityCorrectionResults.IncorrectResult($"Provided quantity entry is not correct. Entry: {entry}");
+    }
+}
--- src/Omniom/Omniom.Tests/Products/ProductsCatalogueSeedingTests.cs
+++ src/Omniom/Omniom.Tests/Products/ProductsCatalogueSeedingTests.cs
@@ -1,18 +1,48 @@
-﻿using Omniom.Tests.Shared;
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Omniom.Domain.ProductsCatalogue.SeedDatabase;
+using Omniom.Tests.Shared;
 
 namespace Omniom.Tests.Products;
 
-public class ProductsCatalogueSeedingTests 
+public class ProductsCatalogueSeedingTests
 {
     private OmniomApp _app = default!;
 
-    [Test]
-    public async Task ShouldSeedDatabaseWithImportData()
+    internal record UnitTranslation(string Unit, decimal ConversionRatio);
+
+
+    [TestCase("320 g (450 ml)", 320)]
+    [TestCase("168 g (3 x 56 g) (przed odsączeniem 240 g (3 x 80 g)", 168)]
+    [TestCase("480 g + 30 g sos", 480)]
+    [TestCase("275 ml", 275)]
+    [TestCase("0,7 l", 700)]
+    public void ShouldConvertProductQuantityAndServingSizeFromAnotherUnitToGrams(string input, int expectedValueInGrams)
     {
-        //var result = await SeedProductsCatalogueHandler.HandleAsync();
+        Assert.That(QuantityCorrector.ConvertQuantitySizeTextToNumericValueSpecifiedInGrams(input).Result, Is.EqualTo(expectedValueInGrams));
+    }
 
-        //Assert.AreEqual(6791, result.SuccessfullyImportedProductsCount);
 
+    [TestCase("2", 2)]
+    [TestCase("10", 10)]
+    [TestCase("350,2", 350)]
+    [TestCase("90.879", 90)]
+    public void ShouldMarkUnitAsGramsWhenNotProvided(string input, int expectedValueInGrams)
+    {
+        Assert.That(QuantityCorrector.ConvertQuantitySizeTextToNumericValueSpecifiedInGrams(input).Result, Is.EqualTo(expectedValueInGrams));
+    }
 
+    
+    [TestCase("10 szt.")]
+    [TestCase("25 sztuk")]
+    [TestCase("1pcs")]
+    [TestCase("8 tranches entames comprises")]
+    [TestCase("185/130")]
+    [TestCase("12pcs")]
+    [TestCase("6 x 4")]
+    [TestCase("5 x 30 g")]
+    [TestCase("270 h")]
+    public void ShouldThrowArgumentExceptionWhenInvalidInputIsProvided(string input)
+    {
+        Assert.That(QuantityCorrector.ConvertQuantitySizeTextToNumericValueSpecifiedInGrams(input).HasError, Is.True);
     }
 }
