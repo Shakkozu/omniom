25cbdf07a19934a74ca23f8cb8dc402bdca69762
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-04-11 20:59:55 +0200

    Add total percents form control. Add error message for invalid input. Fix styling

--- src/omniom-webapp/src/app/user-profile/components/nutrition-targets-configuration/nutrition-targets-configuration.component.ts
+++ src/omniom-webapp/src/app/user-profile/components/nutrition-targets-configuration/nutrition-targets-configuration.component.ts
@@ -14,55 +14,70 @@ import { Store } from '@ngxs/store';
       </mat-card-header>
       <mat-card-content class="mt-4 mx-4">
         <div *ngIf="!(loading$ | async)" class="grid grid-cols-2 gap-4 auto-rows-max hover:auto-rows-min">
-          <div class="col-span-4 col-end-3 col-start-1">
+          <div class="col-span-2">
             <mat-form-field class="w-full">
               <mat-label>Zapotrzebowanie kaloryczne</mat-label>
               <input matInput type="number" placeholder="Kcal" [(ngModel)]="viewModel.kcal">
+              <span matTextSuffix="">Kcal</span>
             </mat-form-field>
           </div>
           <div>
             <mat-form-field class="w-full">
               <mat-label>Białko g</mat-label>
               <input matInput type="number" [(ngModel)]="viewModel.proteinsG">
+              <label matTextSuffix>g</label>
             </mat-form-field>
           </div>
           <div>
             <mat-form-field class="w-full">
               <mat-label>Białko %</mat-label>
               <input matInput type="number" [(ngModel)]="viewModel.proteinsPercent">
+              <label matTextSuffix>%</label>
             </mat-form-field>
           </div>
           <div class="col-start-1 col-span-1">
             <mat-form-field class="w-full">
               <mat-label>Węglowodany g</mat-label>
-              <input matInput type="number" [(ngModel)]="viewModel.carbohydratesPercent">
+              <input matInput type="number" [(ngModel)]="viewModel.carbohydratesG">
+              <label matTextSuffix>g</label>
             </mat-form-field>
           </div>
           <div>
             <mat-form-field class="w-full">
               <mat-label>Węglowodany %</mat-label>
-              <input matInput type="number" [(ngModel)]="viewModel.carbohydratesG">
+              <input matInput type="number" [(ngModel)]="viewModel.carbohydratesPercent">
+              <label matTextSuffix>%</label>
             </mat-form-field>
           </div>
           <div class="col-start-1 col-span-1">
             <mat-form-field class="w-full">
               <mat-label>Tłuszcze g</mat-label>
               <input matInput type="number" [(ngModel)]="viewModel.fatsG">
+              <label matTextSuffix>g</label>
             </mat-form-field>
           </div>
           <div>
             <mat-form-field class="w-full">
               <mat-label>Tłuszcze %</mat-label>
               <input matInput type="number" [(ngModel)]="viewModel.fatsPercent">
+              <label matTextSuffix>%</label>
             </mat-form-field>
           </div>
-      </div>
-      <div class="row mt-8 pe-4 flex flex-row-reverse">
-        <button [disabled]="loading$ | async" (click)="onSaveButtonClicked()" mat-raised-button color="primary">Zapisz</button>
-      </div>
-    </mat-card-content>
-  </mat-card>
-</div>`,
+          <div class="col-start-2">
+            <mat-form-field class="w-full">
+              <mat-label>Łącznie %</mat-label>
+              <input readonly matInput type="number" [(ngModel)]="viewModel.totalPercents">
+              <label matTextSuffix>%</label>
+            </mat-form-field>
+          </div>
+        </div>
+        <mat-error class="my-4 text-lg">Suma % makroskładników musi wynosić 100%</mat-error>
+        <div class="row mt-8 pe-4 flex flex-row-reverse">
+          <button [disabled]="loading$ | async" (click)="onSaveButtonClicked()" mat-raised-button color="primary">Zapisz</button>
+        </div>
+      </mat-card-content>
+    </mat-card>
+  </div>`,
   styleUrl: './nutrition-targets-configuration.component.scss'
 })
 export class NutritionTargetsConfigurationComponent {
@@ -74,7 +89,8 @@ export class NutritionTargetsConfigurationComponent {
     carbohydratesG : 0,
     carbohydratesPercent : 0,
     fatsG : 0,
-    fatsPercent : 0
+    fatsPercent : 0,
+    totalPercents : 100,
   };
 
   
