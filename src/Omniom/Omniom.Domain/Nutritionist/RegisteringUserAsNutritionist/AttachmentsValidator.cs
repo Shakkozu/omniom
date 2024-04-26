0ea485b1c4ca887bd501a821b17e09e349dceca9
adam.kolodziejczyk <shakkozuu@gmail.com>
2024-04-26 16:55:51 +0200

    Add attachments validator and attachments validator tests which will verify whether provided attachments are in correct format and in correct size

---
+++ src/Omniom/Omniom.Domain/Nutritionist/RegisteringUserAsNutritionist/AttachmentsValidator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Omniom.Domain.Nutritionist.RegisteringUserAsNutritionist;
+
+
+public record AttachmentsValidatorConfig(int MaximumAllowedAttachmentSizeInMB, int MaximumAllowedAttachmentsTotalSizeInMB, string[] AllowedAttachmentFormats)
+{
+    public static AttachmentsValidatorConfig Default => new(
+        MaximumAllowedAttachmentSizeInMB: 3,
+        MaximumAllowedAttachmentsTotalSizeInMB: 10,
+        AllowedAttachmentFormats: ["pdf"]);
+}
+
+public record AttachmentsValidationResult
+{
+    public List<string> FilesWithInvalidExtensions { get; init; } = [];
+    public int TooLargeFilesCount { get; init; }
+    public bool TotalSizeExceedsLimit { get; init; }
+
+    public bool IsValid => !TotalSizeExceedsLimit && TooLargeFilesCount == 0 && FilesWithInvalidExtensions.Count == 0;
+}
+public class AttachmentsValidator
+{
+    private AttachmentsValidatorConfig _validatorConfig;
+
+    public AttachmentsValidator(AttachmentsValidatorConfig validatorConfig)
+    {
+        _validatorConfig = validatorConfig;
+    }
+
+    public AttachmentsValidationResult ValidateFiles(string[] files)
+    {
+        var filesWithInvalidExtensions = files.Where(file => !file.StartsWith("data:application/pdf;base64,")).ToList();
+        var tooLargeFilesCount = files.Count(file =>
+        {
+            var base64Data = Regex.Match(file, @"data:application/(?<type>.+?),(?<data>.+)").Groups["data"].Value;
+            var binData = Convert.FromBase64String(base64Data);
+            return binData.Length > _validatorConfig.MaximumAllowedAttachmentSizeInMB * 1024 * 1024;
+        });
+        var totalSizeExceedsLimit = files.Sum(file =>
+        {
+            var base64Data = Regex.Match(file, @"data:application/(?<type>.+?),(?<data>.+)").Groups["data"].Value;
+            var binData = Convert.FromBase64String(base64Data);
+            return binData.Length;
+        }) > _validatorConfig.MaximumAllowedAttachmentsTotalSizeInMB * 1024 * 1024;
+
+        return new AttachmentsValidationResult
+        {
+            FilesWithInvalidExtensions = filesWithInvalidExtensions,
+            TooLargeFilesCount = tooLargeFilesCount,
+            TotalSizeExceedsLimit = totalSizeExceedsLimit
+        };
+    }
+}
---
+++ src/Omniom/Omniom.Tests/Nutritionist/AttachmentsValidationTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using Omniom.Domain.Nutritionist.RegisteringUserAsNutritionist;
+using System;
+namespace Omniom.Tests.Nutritionist;
+
+[TestFixture]
+public class FileValidatorTests
+{
+    [Test]
+    public void ValidationShouldFailWhenAttachmentSizeExceedsMaximumAllowedAttachmentSize()
+    {
+        var validatorConfig = AttachmentsValidatorConfig.Default;
+        var validator = new AttachmentsValidator(validatorConfig);
+        var megabyte = 1024 * 1024;
+        var largePdfFile = "data:application/pdf;base64," + Convert.ToBase64String(new byte[validatorConfig.MaximumAllowedAttachmentSizeInMB * megabyte + 1]);
+
+        var result = validator.ValidateFiles(new string[] { largePdfFile });
+
+        Assert.That(result.TooLargeFilesCount, Is.EqualTo(1));
+        Assert.That(result.IsValid, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void ValidationShouldFailWhenAttachmentsTotalSizeExceedsMaximumAllowedAttachmentsTotalSize()
+    {
+        var validatorConfig = AttachmentsValidatorConfig.Default;
+        var validator = new AttachmentsValidator(validatorConfig);
+        var megabyte = 1024 * 1024;
+        var largePdfFile = "data:application/pdf;base64," + Convert.ToBase64String(new byte[validatorConfig.MaximumAllowedAttachmentSizeInMB * megabyte - 1]);
+        var files = new string[] { largePdfFile, largePdfFile, largePdfFile, largePdfFile };
+
+        var result = validator.ValidateFiles(files);
+
+        Assert.That(result.TotalSizeExceedsLimit, Is.EqualTo(true));
+        Assert.That(result.IsValid, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void ValidationShouldFailWhenAttachmentsContainsNotAllowedFormat()
+    {
+        var validatorConfig = AttachmentsValidatorConfig.Default;
+        var validator = new AttachmentsValidator(validatorConfig);
+        var files = new string[] { "data:application/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==" };
+
+        var result = validator.ValidateFiles(files);
+
+        Assert.That(result.FilesWithInvalidExtensions.Count, Is.EqualTo(1));
+        Assert.That(result.IsValid, Is.EqualTo(false));
+    }
+
+    [Test]
+    public void ValidationShouldPassWhenFileExtensionAndSizeIsValid()
+    {
+        var validatorConfig = AttachmentsValidatorConfig.Default;
+        var validator = new AttachmentsValidator(validatorConfig);
+        var validPdfFile = "data:application/pdf;base64," + new String('A', 3 * 1024 * 1024);
+        var files = new string[] { validPdfFile };
+
+        var result = validator.ValidateFiles(files);
+
+        Assert.That(result.FilesWithInvalidExtensions.Count, Is.EqualTo(0));
+        Assert.That(result.TooLargeFilesCount, Is.EqualTo(0));
+        Assert.That(result.TotalSizeExceedsLimit, Is.EqualTo(false));
+        Assert.That(result.IsValid, Is.EqualTo(true));
+    }
+}
